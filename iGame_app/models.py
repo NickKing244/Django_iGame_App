27b6c8dfad473fc42932dc4cee2c7b62c64df74e
Nick King <nickking244@gmail.com>
2021-09-14 09:29:42 -0400

    added css and views

--- iGame_app/models.py
+++ iGame_app/models.py
@@ -30,15 +30,32 @@ class UserManager(models.Manager):
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email']):
             errors['regex'] = "Invalid Email Address"
-        users_with_email = User.objects.filter(email=postData['email'])
-        if len(users_with_email) >= 1:
-            errors['dupe'] = "Email taken, use another"
+        if len(postData['email']) < 4:
+            errors['email'] = "Email is too short"
+        if len(postData['password']) < 8:
+            errors['password'] = "Password must be at least 8 characters"
+        return errors
+
+class GameManager(models.Manager):
+    def game_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title must be at least 2 characters"
+        if len(postData['developer']) < 2:
+            errors['developer'] = "Developer must be at least 2 characters"
+        if len(postData['publisher']) < 2:
+            errors['publisher'] = "Publisher must be at least 2 characters"
+        if len(postData['platform']) < 2:
+            errors['platform'] = "Platform must be at least 2 characters"
+        if len(postData['genre']) < 2:
+            errors['genre'] = "Genre must be at least 2 characters"
+        if len(postData['summary']) < 2:
+            errors['summary'] = "Summary must be at least 2 characters"
         return errors
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -55,3 +72,4 @@ class Game(models.Model):
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = GameManager()
--- iGame_app/views.py
+++ iGame_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
 
 # Login/Resgister
 
@@ -6,35 +9,90 @@ def index(request):
     return render(request, 'index.html')
 
 def create_user(request):
-    pass
+    if request.method == 'POST':
+        errors = User.objects.create_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            password = request.POST['password']
+            pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+            user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], username=request.POST['username'], email=request.POST['email'], password=pw_hash)
+            request.session['userid'] = User.objects.last().id
+            print(request.session['userid'])
+            return redirect('/games/dashboard')
+    return redirect('/')
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        users_with_email = User.objects.filter(email=request.POST['email'])
+        if users_with_email:
+            user = users_with_email[0]
+            request.session['userid'] = user.id
+            print(request.session['userid'])
+            if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+                return redirect('/games/dashboard')
+        messages.error(request, "Email or password are not correct")
+    return redirect('/')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
 
 # Dashboard
 
 def all_games(request):
-    pass
+    context = {
+        'user': User.objects.get(id=request.session['userid']),
+        'all_games': Game.objects.all(),
+    }
+    if 'userid' not in request.session:
+        return redirect('/')
+    else:
+        return render(request, 'dashboard.html', context)
 
-def unfavorite_game(request):
-    pass
+def unfavorite_game(request, game_id):
+    if 'userid' not in request.session and request.method == 'POST':
+        return redirect('/')
+    else:
+        this_game = Game.objects.get(id=game_id)
+        this_game.delete()
+    return redirect('/games/dashboard')
 
-def render_game_details(request):
-    pass
-
-def render_search_page(request):
-    pass
+def render_game_details(request, game_id):
+    context = {
+        'game': Game.objects.get(id=game_id)
+    }
+    return render(request, 'details.html', context)
 
 def render_account_page(request):
-    pass
+    context = {
+        'user': User.objects.get(id=request.session['userid'])
+    }
+    if 'userid' not in request.session:
+        return redirect('/')
+    else:
+        return render(request, 'account.html', context)
 
-# Search Page
+def render_add_game(request):
+    if 'userid' not in request.session:
+        return redirect("/")
+    else:
+        return render(request, 'add_game.html')
 
-def search_games(request):
-    pass
+# Add Game Page
 
-def favorite_game(request):
-    pass
+def create_game(request):
+    this_user = User.objects.get(id=request.session['userid'])
+    errors = Game.objects.game_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/games/new')
+    else:
+        Game.objects.create(title=request.POST['title'], developer=request.POST['developer'], publisher=request.POST['publisher'], genre=request.POST['genre'], summary=request.POST['summary'], release_date=request.POST['release_date'], platform=request.POST['platform'])
+        return redirect('/games/dashboard')
 
 # Details Page
 
@@ -44,7 +102,17 @@ def add_note(request):
 # Account Page
 
 def update_account(request):
-    pass
-
-def logout(request):
-    pass
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/users/account')
+    if 'userid' not in request.session:
+        return redirect('/')
+    else:
+        this_user = User.objects.get(id=request.session['userid'])
+        this_user.first_name = request.POST['first_name']
+        this_user.last_name = request.POST['last_name']
+        this_user.email = request.POST['email']
+        this_user.save()
+        return redirect('/users/account')
